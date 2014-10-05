793293bd7cbc9c0ba975b85e939c8f7f7b800c82
brett <beergeek@tpg.com.au>
2014-10-06 07:18:01 +1100

    make usable for other people

--- manifests/base.pp
+++ manifests/base.pp
@@ -1,59 +1,58 @@
 class scoping::base {
 
 
-  host { 'ca.puppetlabs.vm':
+  host { $::fqdn:
     ensure       => 'present',
     host_aliases => ['ca'],
     ip           => '192.168.62.194',
-    target       => '/etc/hosts',
+    tag          => $::fqdn,
   }
   @host { 'h0.puppetlabs.vm':
     ensure       => 'present',
     host_aliases => ['h0'],
     ip           => '192.168.62.200',
-    target       => '/etc/hosts',
+    tag          => 'h0.puppetlabs.vm',
   }
   @host { 'lb.puppetlabs.vm':
     ensure       => 'present',
     host_aliases => ['lb'],
     ip           => '192.168.62.201',
-    target       => '/etc/hosts',
+    tag          => 'lb.puppetlabs.vm',
   }
   host { 'localhost':
     ensure       => 'present',
     host_aliases => ['localhost.localdomain', 'localhost4', 'localhost4.localdomain4'],
     ip           => '127.0.0.1',
-    target       => '/etc/hosts',
   }
   @host { 'm0.puppetlabs.vm':
     ensure       => 'present',
     host_aliases => ['m0'],
     ip           => '192.168.62.195',
-    target       => '/etc/hosts',
+    tag          => 'm0.puppetlabs.vm',
   }
   @host { 'm1.puppetlabs.vm':
     ensure       => 'present',
     host_aliases => ['m1'],
     ip           => '192.168.62.196',
-    target       => '/etc/hosts',
+    tag          => 'm1.puppetlabs.vm',
   }
   @host { 'pdb.puppetlabs.vm':
     ensure       => 'present',
     host_aliases => ['pdb'],
     ip           => '192.168.62.197',
-    target       => '/etc/hosts',
+    tag          => 'pdb.puppetlabs.vm',
   }
   @host { 's0.puppetlabs.vm':
     ensure       => 'present',
     host_aliases => ['s0'],
     ip           => '192.168.62.198',
-    target       => '/etc/hosts',
+    tag          => 's0.puppetlabs.vm',
   }
   @host { 's1.puppetlabs.vm':
     ensure       => 'present',
     host_aliases => ['s1'],
     ip           => '192.168.62.199',
-    target       => '/etc/hosts',
+    tag          => 's1.puppetlabs.vm',
   }
 
   resources { 'host':
